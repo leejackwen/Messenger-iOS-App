0745894d30e3be29bc404a053262e56c22482ffe
Jack <Jack@Jacks-MacBook-Air-5.local>
2020-06-16 22:56:58 +0100

    Getting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Jack on 16/06/2020.
+//  Copyright © 2020 Jack Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
