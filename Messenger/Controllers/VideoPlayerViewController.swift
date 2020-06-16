a3e737f3065fcf4d11bcce8f95da26bab800f106
Jack <Jack@Jacks-MacBook-Air-5.local>
2020-06-16 23:06:31 +0100

    Wrap up getting started

---
+++ Messenger/Controllers/VideoPlayerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoPlayerViewController.swift
+//  Messenger
+//
+//  Created by Jack on 16/06/2020.
+//  Copyright © 2020 Jack Lee. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
