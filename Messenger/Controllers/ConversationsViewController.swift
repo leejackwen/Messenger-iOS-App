1d888b433d1ec5828e7c1b9784bcca97f0cd98c1
Jack <Jack@Jacks-MacBook-Air-5.local>
2020-06-20 21:27:09 +0100

    Handle auth session, add database

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import Firebase
 
 class ConversationsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
+        validateAuth()
 
+    }
+    
+    private func validateAuth() {
+        if Firebase.Auth.auth().currentUser == nil {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+    
+    
+    
+    
+    
 }
 
---
+++ Messenger/Resources/Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Extensions.swift
+//  Messenger
+//
+//  Created by Jack on 16/06/2020.
+//  Copyright © 2020 Jack Lee. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+    
+}
